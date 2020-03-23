deb42507d93870c2ed4fcbb2be829abed9fda5a1
ilunga lebreton <ilungalebreton@gmail.com>
2020-03-23 03:06:33 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+    'url' : "mongodb+srv://lebretoni:Idl2014.@stackpersonal-amnzv.mongodb.net/test?retryWrites=true&w=majority", // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'fullstack'
+};
